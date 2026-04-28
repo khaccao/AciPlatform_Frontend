f149bcc1a9f7a925f392ab8d29600be5eb59aab6
khaccao <nguyenkhaccao1@gmail.com>
2026-04-28 16:26:19 +0700

    feat: Pass auth token to Sell iframe

--- src/features/sell/pages/SellPage.tsx
+++ src/features/sell/pages/SellPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 export const SellPage: React.FC = () => {
+    // Lấy token hiện tại của hệ thống để truyền sang Iframe
+    const token = localStorage.getItem('token');
     // URL của subdomain độc lập chứa Frontend Bán hàng
-    const sellModuleUrl = 'https://sit.sell.nguyenbinh.info.vn';
+    const sellModuleUrl = token ? `https://sit.sell.nguyenbinh.info.vn/?token=${token}` : 'https://sit.sell.nguyenbinh.info.vn/';
 
     return (
         <div style={{ width: '100%', height: 'calc(100vh - 64px)', overflow: 'hidden' }}>
